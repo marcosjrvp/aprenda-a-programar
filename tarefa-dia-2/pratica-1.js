6e96c5a57cb0ed81b4db3cfbc3c2bf0600db51ca
Marcos Junior <marcosjr_rj@hotmail.com>
2022-08-25 13:39:46 -0300

    console.log efetuado nas variáveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,3 +10,9 @@ email = "marcosjr_rj@hotmail.com"
 profissao = "Analista de Suporte"
 idade = 34
 lugardesejado = "Disney"
+
+console.log("O meu nome é :", nome)
+console.log("O meu email é :", email)
+console.log("A minha profissão é :", profissao)
+console.log("A minha idade é :", idade)
+console.log("O lugar que eu desejo conhecer é :", lugardesejado)
