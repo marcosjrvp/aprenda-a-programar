52cff04bce54da8331197a51a2f69bb003fd7087
Marcos Junior <marcosjr_rj@hotmail.com>
2022-08-28 12:31:58 -0300

    Envio da tarefa 3 - prática 3

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,16 @@
+let i
+let n
+let verificarNumerosImpares
+
+i = 1
+n = 16
+
+while(i <= n){
+
+       if(verificarNumerosImpares = i % 2 != 0) 
+     
+           console.log(i)
+        i = i + 1
+    
+   
+}
