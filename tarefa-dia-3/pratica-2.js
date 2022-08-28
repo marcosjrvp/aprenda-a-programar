2ae976f8da6700c867f5c51a203e7bb5e1d1d248
Marcos Junior <marcosjr_rj@hotmail.com>
2022-08-28 12:28:48 -0300

    Envio da tarefa 2 - prática 2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,18 @@
+let multiplicador
+let i
+let n
+let resultadoTabuada
+
+
+multiplicador = 9
+i = 1
+n = 10
+
+
+while(i<=n){
+    
+    resultadoTabuada = multiplicador * i
+    console.log(`${multiplicador} x ${i} = ${resultadoTabuada}`);
+    i = i + 1
+    
+}
